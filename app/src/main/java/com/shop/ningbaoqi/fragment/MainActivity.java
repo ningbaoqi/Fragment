37495f35d929bdb965f8cf7ebf7f6784946bd2a8
ningbaoqi <baoqi.ning@new-bund.net>
2018-07-19 17:35:46 +0800

    Fragment动态加载

--- app/src/main/java/com/shop/ningbaoqi/fragment/MainActivity.java
+++ app/src/main/java/com/shop/ningbaoqi/fragment/MainActivity.java
@@ -1,5 +1,6 @@
 package com.shop.ningbaoqi.fragment;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +26,11 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                 startActivity(intent);
                 break;
             case R.id.second:
+                MyFragment2 myFragment2 = new MyFragment2();
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.add(R.id.frame, myFragment2);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
                 break;
             case R.id.third:
                 break;
